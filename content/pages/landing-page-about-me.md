f7369db14720897a4e59cf6eeab12c226e6e6501
ashwinschronicles <ashwinschronicles>
2020-03-22 18:30:58 +0530

    $1

---
+++ content/pages/landing-page-about-me.md
@@ -0,0 +1,25 @@
+---
+author: Ashwin
+title: What is this place?
+layout: page
+date: 2018-06-18
+status: hidden
+slug: landing-page-about-hidden
+---
+
+
+Thanks for joining me!
+
+I am Ashwin Kumar K, a Dual Major (B.E Electronics and Instrumentation & Int. MSc Physics) student at BITS Pilani KK Birla Goa campus (Batch of 2017)
+
+I had always wanted to publish some of my projects and experiments on a public platform so that people working on similar projects can give and take ideas. It also serves as a way to showcase the experience that I have gained while doing such projects.
+
+I intend to post details of projects or experiments that I have performed  mostly in the field of Science and Engineering as I complete them on this web-page.
+
+Take a trip down my memory lane. I hope to see you around!
+
+Here is my CV  :
+
+[1 page version]({filename}/pdfs/Academic_CV.pdf)
+
+[Curriculum Vitae]({filename}/pdfs/Vitae.pdf)
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,27 +4,26 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Ashwin Kumar K'
 SITENAME = u"""<span style="color:#329A97;">Ashwin's</span> <span style="color:purple">Chronicles</span> """
-SITEURL = '/'
+SITEURL = ''
 #SITENAME = u"Ashwin's Chronicles"
 #SITEURL = 'https://ashwinschronicles.github.io/'
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Calcutta'
+DATE_FORMATS = {"en": "%b %d, %Y"}
 THEME = 'theme'
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
 # Feeds
-FEED_ALL_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-#FEED_ALL_ATOM = 'feeds/all.atom.xml'
-#FEED_ALL_RSS = 'feeds/all.rss.xml'
-#AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
-#RSS_FEED_SUMMARY_ONLY = False
+CATEGORY_FEED_RSS = None
 
-STATIC_PATHS = ['images', 'pdfs','extra']
+
+STATIC_PATHS = ["theme/images", "images", "extra/_redirects", "code", "pdf"]
 
 EXTRA_PATH_METADATA = {
     'extra/custom.css': {'path': 'custom.css'},
@@ -33,6 +32,15 @@ EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
     'extra/LICENSE': {'path': 'LICENSE'},
     'extra/README': {'path': 'README'},
+}
+MARKDOWN = {
+    "extension_configs": {
+        "markdown.extensions.admonition": {},
+        "markdown.extensions.codehilite": {"css_class": "highlight"},
+        "markdown.extensions.extra": {},
+        "markdown.extensions.meta": {},
+        "markdown.extensions.toc": {"permalink": " "},
+    }
 }
 
 # Blogroll
@@ -45,64 +53,106 @@ SOCIAL = (('Git', 'https://github.com/iamashwin99/'),
           ('Facebook', 'https://www.facebook.com/iamashwin99'),
           ('LinkedIn','https://www.linkedin.com/in/ashwin-k-4854b8121/'))
 
-# clean urls for pages , trailing slash to support HTTPS
-PAGE_URL = '{slug}/'
-PAGE_SAVE_AS = '{slug}/index.html'
-# clean urls for articles
-ARTICLE_SAVE_AS = '{slug}/index.html'
-ARTICLE_URL = '{slug}/'
+# Defaults
+DEFAULT_CATEGORY = "Miscellaneous"
+USE_FOLDER_AS_CATEGORY = False
+ARTICLE_URL = "{slug}"
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}.html"
+TAGS_URL = "tags"
+CATEGORIES_URL = "categories"
+ARCHIVES_URL = "archives"
+SEARCH_URL = "search"
+TAG_SAVE_AS = ""
+AUTHOR_SAVE_AS = ""
+CATEGORY_SAVE_AS = ""
+USE_SHORTCUT_ICONS = True
 
-TYPOGRIFY = False
-DEFAULT_PAGINATION = 10
+
+TYPOGRIFY = True
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 PLUGIN_PATHS = ['/home/ashwin/Data/Sandbox/website/plugins/']
-PLUGINS = ['tipue_search' , 'pelican-toc','neighbors','render_math', 'sitemap','more_categories','liquid_tags.img','neighbors', 'related_posts', 'share_post',
-           'series']
-DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+
+PLUGINS = ["tipue_search",
+           "pelican-toc",
+           "neighbors",
+           "render_math", 
+           "sitemap",
+           "more_categories",
+           "liquid_tags.include_code",
+           "liquid_tags.img",
+           "neighbors",
+           "related_posts", 
+           "share_post", 
+           "series", 
+           "post_stats", 
+           "pelican-js" ]           
+DIRECT_TEMPLATES = ["index", "tags", "categories", "archives", "search", "404"]
 
 
 SITEMAP = {
-    'format': 'xml',
-    'priorities': {
-        'articles': 0.5,
-        'indexes': 0.5,
-        'pages': 0.5
-     },
-   'changefreqs': {
-        'articles': 'monthly',
-        'indexes': 'daily',
-        'pages': 'monthly'
-    }
+    "format": "xml",
+    "priorities": {"articles": 0.5, "indexes": 0.5, "pages": 0.5},
+    "changefreqs": {"articles": "monthly", "indexes": "daily", "pages": "monthly"},
 }
+
 
 
 #### render_math settings
 MATH_JAX = {'color':'red','align':'centre','mathjax_font':'sanserif','linebreak_automatic':'True','tex_extensions': ['color.js','mhchem.js']}
 
 # Elegant Labels
-SOCIAL_PROFILE_LABEL = u'Stay in Touch'
-RELATED_POSTS_LABEL = 'Keep Reading'
-SHARE_POST_INTRO = 'Like this post? Share on:'
-COMMENTS_INTRO = u'So what do you think? Did I miss something? Is any part unclear? Leave your comments below.'
+SOCIAL_PROFILE_LABEL = "Stay in Touch"
+RELATED_POSTS_LABEL = "Keep Reading"
+SHARE_POST_INTRO = "Like this post? Share on:"
+COMMENTS_INTRO = "So what do you think? Did I miss something? Is any part unclear? Leave your comments below."
 
-# Mailchimp
-#EMAIL_SUBSCRIPTION_LABEL = u'Get Monthly Updates'
-#EMAIL_FIELD_PLACEHOLDER = u'Enter your email...'
-#SUBSCRIBE_BUTTON_TITLE = u'Send me Free updates'
-#MAILCHIMP_FORM_ACTION = u'empty'
+# Email Subscriptions
+EMAIL_SUBSCRIPTION_LABEL = "Get New Release Alert"
+EMAIL_FIELD_PLACEHOLDER = "Enter your email..."
+SUBSCRIBE_BUTTON_TITLE = "Notify me"
+
+FREELISTS_NAME = "oracle-l"
+FREELISTS_FILTER = True
+
+# SMO
+TWITTER_USERNAME = "@iamashwin99"
+FEATURED_IMAGE = SITEURL + "/theme/images/final.png"
+
+# Share links at bottom of articles
+# Supported: twitter, facebook, hacker-news, reddit, linkedin, email
+SHARE_LINKS = [("twitter", "Twitter"), ("facebook", "Facebook"), ("email", "Email")]
 
 # Legal
 SITE_LICENSE = """All articles written by me in this website are licensed under <a rel="license" 
     href="http://creativecommons.org/licenses/by/4.0/">
-    Creative Commons Attribution 4.0 International License</a>. <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>""" 
+    Creative Commons Attribution 4.0 International License</a>.""" 
     
 # Landing Page
-PROJECTS = [{'name': 'Cryostat Probe design and optimisation for transport measurement.', 'url': 'https://ashwinschronicles.github.io/null/','description': 'Design of a cryogenic probe for transport measurements and using it to observe Superconducting transition of Niobium Nitrate'},
-            {'name': 'A review of Analog Discovery 2', 'url': 'https://ashwinschronicles.github.io/null/','description': 'A review of Analog discovery 2 in a Lab environment'},
-            {'name': 'A Beginers Guide to IoT', 'url': 'https://ashwinschronicles.github.io/Intro-to-IoT/','description': 'An OTA controlled light'},
-            {'name': 'A Pressure-sensitive mat', 'url': 'https://ashwinschronicles.github.io/null/','description': 'Designed a pressure-sensitive mat that can sense touch enabling the determination of different poses such as Running, Jumping,Rightward-leftward movement,one-leg hop etc.'}]
+PROJECTS = [{
+    'name': 'Cryostat Probe design and optimisation for transport measurement.',
+    'url': 'https://ashwinschronicles.github.io/null/',
+    'description': 'Design of a cryogenic probe for transport measurements and using it to observe Superconducting transition of Niobium Nitrate'
+  },
+  {
+    'name': 'A review of Analog Discovery 2',
+    'url': 'https://ashwinschronicles.github.io/null/',
+    'description': 'A review of Analog discovery 2 in a Lab environment'
+  },
+  {
+    'name': 'A Beginers Guide to IoT',
+    'url': 'https://ashwinschronicles.github.io/Intro-to-IoT/',
+    'description': 'An OTA controlled light'
+  },
+  {
+    'name': 'A Pressure-sensitive mat',
+    'url': 'https://ashwinschronicles.github.io/null/',
+    'description': 'Designed a pressure-sensitive mat that can sense touch enabling the determination of different poses such as Running, Jumping,Rightward-leftward movement,one-leg hop etc.'
+  }
+]
 
 LANDING_PAGE_ABOUT = {'title': 'The Journey Begins',
                       'details': """<p>Thanks for joining me!</p>
@@ -113,14 +163,15 @@ LANDING_PAGE_ABOUT = {'title': 'The Journey Begins',
 <p> Here is my CV : </p>
 <p> <a href="https://ashwinschronicles.github.io/pdfs/Vitae.pdf">Curriculum Vitae</a></p>
 <p> <a href="https://ashwinschronicles.github.io/pdfs/Academic_CV.pdf">1 Page Version</a></p>
-<div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="ashwin-k-4854b8121"><a class="LI-simple-link" href='https://in.linkedin.com/in/ashwin-k-4854b8121?trk=profile-badge'>Ashwin K.(LinkedIn)</a></div>
+
 """}
 
-
+LANDING_PAGE_TITLE = "The Journey Begins"
 # SEO
 SITE_DESCRIPTION = u'Im Ashwin Kumar K,currently dual majoring in Physics and Electronics and Instrumentation from BITS Pilani Goa Campus. This is my personal blog.'
 
 
 #MISC
-USE_FOLDER_AS_CATEGORY = True
-DEFAULT_CATEGORY = 'misc'
+DISQUS_FILTER = True
+UTTERANCES_FILTER = True
+COMMENTBOX_FILTER = True
